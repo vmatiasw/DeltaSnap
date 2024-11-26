332e9f80b5ae9655571f1a2a5b105ade9b70aca9
Matias Viola <matiasvioladi@gmail.com>
2024-11-26 19:52:33 -0300

    refact: mas seguridad para no modificar la db de test

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,19 +10,26 @@ logging.getLogger().level = logging.INFO
 # Si se quiere ver los queries, cambiar a INFO
 logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
 
+DB_PATH = os.path.join('tests/tools/db/test.db')
+
+
 @pytest.fixture(scope='session', autouse=True)
 def setup_db():
-    if not os.path.exists('tests/tools/db/test.db'):
+    if not os.path.exists(DB_PATH):
         from tests.tools.db.db_setup import Base, engine
         from tests.tools.test_db import setup_test_db
         Base.metadata.create_all(engine)
         setup_test_db()
+        os.chmod(DB_PATH, 0o444)  # Permisos de solo lectura (r-- r-- r--)
     yield
+
 
 @pytest.fixture(scope='function')
 def test_session():
+    os.chmod(DB_PATH, 0o644)  # Volver a permisos de escritura (rw- rw- rw-)
     session = localSession()
     session.begin()
     yield session
     session.rollback()
     session.close()
+    os.chmod(DB_PATH, 0o444)  # Permisos de solo lectura (r-- r-- r--)
