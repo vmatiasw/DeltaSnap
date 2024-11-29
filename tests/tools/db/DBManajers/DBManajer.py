bfec71cff7e4abafc10361577e715241726b0704
Matias Viola <matiasvioladi@gmail.com>
2024-11-29 15:48:03 -0300

    refact: DBManajer con mejores tipos y menos acoplado

--- tests/tools/db/DBManajers/DBManajer.py
+++ tests/tools/db/DBManajers/DBManajer.py
@@ -1,6 +1,8 @@
 import os
 from abc import ABC, abstractmethod
 
+HOST = "localhost"
+PORT = ''
 PASSWORD = "test_password"
 USERNAME = "test_user"
 DATABASE = "mysql" # con sqlalchemy: sqlite | mysql | postgresql
@@ -9,26 +11,27 @@ DATABASE_PATH = os.path.join(os.path.dirname(__file__), "test.db")
 
 class DBManager(ABC):
     def __init__(self):
+        self.host = HOST
+        self.port = PORT
         self.database = DATABASE
         self.username = USERNAME
         self.password = PASSWORD
         self.db_name = DATABASE_NAME
         self.db_path = self._get_database_url()
-        self.engine = self._create_engine()
-        self.sessionMaker = self._create_sessionMaker()
-        self.Base = self._create_base()
-
-    @abstractmethod
-    def _get_database_url(self): ...
-
-    @abstractmethod
-    def _create_engine(self): ...
-
-    @abstractmethod
-    def _create_sessionMaker(self): ...
     
-    @abstractmethod
-    def _create_base(self): ...
+    @staticmethod
+    def _get_database_url() -> str:
+        """Devuelve la URL para conectar con la base de datos."""
+        port = f":{PORT}" if PORT else ""
+        match DATABASE:
+            case "sqlite":
+                return f"sqlite:///{DATABASE_PATH}"
+            case "mysql":
+                return f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}{port}/{DATABASE_NAME}"
+            case "postgresql":
+                return f"postgresql://{USERNAME}:{PASSWORD}@{HOST}{port}/{DATABASE_NAME}"
+            case _:
+                raise Exception(f"Database {DATABASE} not supported")
     
     @abstractmethod
     def get_base(self): ...
@@ -37,7 +40,7 @@ class DBManager(ABC):
     def get_newSession(self): ...
 
     @abstractmethod
-    def create_tables(self): ...
+    def create_tables(self) -> None: ...
 
     @abstractmethod
-    def drop_tables(self): ...
+    def drop_tables(self) -> None: ...
