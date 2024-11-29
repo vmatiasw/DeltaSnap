52b9ed58c8af1eb73393efd963ef329baac7411b
Matias Viola <matiasvioladi@gmail.com>
2024-11-29 19:47:35 -0300

    refact: mejora de nomre de variables, no se menciona adapter

--- DeltaDB/data_processing/ingestion.py
+++ DeltaDB/data_processing/ingestion.py
@@ -1,7 +1,32 @@
 from collections import defaultdict
 
-from tests.tools.db.database_connector import db_metedata_adapter
-from DeltaDB.types import Capture#, ObjectsList
+from tests.tools.db.database_connector import db_metedata_adapter as db_metedata
+from DeltaDB.types import Capture
+
+def capture_all_tables(session, page_size: int = 1) -> Capture:
+    tables = db_metedata.get_tables()
+    metadata: Capture = defaultdict(dict)
+
+    for table in tables:
+        offset = 0
+        columns = db_metedata.get_columns(table)
+        table_name = db_metedata.get_table_name(table)
+        while instances := db_metedata.get_instances(session, table, offset, page_size):
+            for record in instances:
+                record_id = db_metedata.get_record_id(record)
+                for column in columns:
+                    column_name = db_metedata.get_column_key(column)
+                    column_value = db_metedata.get_column_value(column_name, record)
+                    column_is_foreign_key = db_metedata.column_is_foreign_key(column)
+
+                    key = f'{column_name} (FK)' if column_is_foreign_key else column_name
+                    metadata[table_name, record_id][key] = column_value
+            
+            offset += page_size
+            
+    return dict(metadata)
+
+#, ObjectsList # TODO: Actualizar esta funcion
 # from DeltaDB.validations import validate_data, ValidateId, ValidateInstance, ValidateTablename
 #
 # # List of validation rules to apply to objects
@@ -23,26 +48,3 @@ from DeltaDB.types import Capture#, ObjectsList
 #             metadata[obj.__tablename__, obj.id][key] = column_value
 
 #     return dict(metadata)
-
-def capture_all_tables(session, page_size: int = 1) -> Capture:
-    tables = db_metedata_adapter.get_tables()
-    metadata: Capture = defaultdict(dict)
-
-    for table in tables:
-        offset = 0
-        columns = db_metedata_adapter.get_columns(table)
-        table_name = db_metedata_adapter.get_table_name(table)
-        while instances := db_metedata_adapter.get_instances(session, table, offset, page_size):
-            for record in instances:
-                record_id = db_metedata_adapter.get_record_id(record)
-                for column in columns:
-                    column_name = db_metedata_adapter.get_column_key(column)
-                    column_value = db_metedata_adapter.get_column_value(column_name, record)
-                    column_is_foreign_key = db_metedata_adapter.column_is_foreign_key(column)
-
-                    key = f'{column_name} (FK)' if column_is_foreign_key else column_name
-                    metadata[table_name, record_id][key] = column_value
-            
-            offset += page_size
-            
-    return dict(metadata)
