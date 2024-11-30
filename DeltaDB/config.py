dfe44a866d7f723b9dd21dc3b8c34abf93aea0d8
Matias Viola <matiasvioladi@gmail.com>
2024-11-30 14:54:35 -0300

    .

--- DeltaDB/config.py
+++ DeltaDB/config.py
@@ -5,7 +5,7 @@ HOST = "localhost"
 PORT = ''
 PASSWORD = "test_password"
 USERNAME = "test_user"
-DATABASE = "mysql" # sqlite | mysql | postgresql
+DATABASE = "sqlite" # sqlite | mysql | postgresql
 DATABASE_NAME = "test_db"
 
 APP_LABEL = "test_app" # Nombre de la aplicación de Django
