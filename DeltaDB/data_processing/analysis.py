dd162eae648128aaf0c45bbb019fd3c1dabcb2b0
Matias Viola <matiasvioladi@gmail.com>
2024-12-01 11:28:03 -0300

    refact: optimizacion analisys

--- DeltaDB/data_processing/analysis.py
+++ DeltaDB/data_processing/analysis.py
@@ -3,47 +3,44 @@ from typing import Tuple
 from DeltaDB.types import Capture, CreatedTables, DeletedTables, FieldsChanges, TablesChanges, info
 from DeltaDB.data_processing.data_classes import Changes, Created, Deleted
 
-# TODO: Al marcar algo se usa # en el valor, ... ¿es necesario? ¿es una buena práctica?
-
 def diff_captures(initial_capture: Capture, final_capture: Capture) -> Tuple[Changes, Created, Deleted]:
     """
     Compares two capture dictionaries and identifies the differences between them, 
     categorizing them into changes, deletions, and creations.
     """
     changes: TablesChanges = {}
-    deleted: DeletedTables = set()
-    created: CreatedTables = set()
+    deleted: DeletedTables = set(initial_capture.keys()) - set(final_capture.keys())
+    created: CreatedTables = set(final_capture.keys()) - set(initial_capture.keys())
 
-    # Detect deleted tables and analyze changes
+    # Detect changes in existing tables
     for table_key, initial_table in initial_capture.items():
         final_table = final_capture.get(table_key)
-        if final_table is None:
-            # Table was deleted
-            deleted.add(table_key)
-            continue
+        if not final_table:
+            continue  # This table is in the deleted set
 
-        # Track changes within the table
         current_changes: FieldsChanges = {}
+        initial_columns = set(initial_table.keys())
+        final_columns = set(final_table.keys())
 
-        for column, initial_value in initial_table.items():
-            if column not in final_table:
-                current_changes[column] = (initial_value, info("column don't exist"))
-                continue
-            
-            final_value = final_table.get(column)
+        # Columns that were removed from the final capture
+        removed_columns = initial_columns - final_columns
+        for column in removed_columns:
+            current_changes[column] = (initial_table[column], info("column don't exist"))
+
+        # Columns that were added to the final capture
+        added_columns = final_columns - initial_columns
+        for column in added_columns:
+            current_changes[column] = (info("column don't exist"), final_table[column])
+
+        # Columns that have changed
+        common_columns = initial_columns & final_columns
+        for column in common_columns:
+            initial_value = initial_table[column]
+            final_value = final_table[column]
             if initial_value != final_value:
                 current_changes[column] = (initial_value, final_value)
-                
-        for column, final_value in final_table.items():
-            if column not in initial_table:
-                current_changes[column] = (info("column don't exist"), final_value)
 
         if current_changes:
             changes[table_key] = current_changes
-
-    # Detect newly created tables
-    for table_key in final_capture.keys():
-        if table_key not in initial_capture:
-            created.add(table_key)
 
     return Changes(changes), Created(created), Deleted(deleted)
