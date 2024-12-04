99972d2da2bbdaabfc246e8b5075b8ab22abc549
Matias Viola <matiasvioladi@gmail.com>
2024-12-04 01:38:03 -0300

    refact: cambios de nombres internos

--- src/domain/data_processing/ingestion.py
+++ src/domain/data_processing/ingestion.py
@@ -1,53 +1,53 @@
 from collections import defaultdict
-from typing import Any, List
+from typing import Any, List, Dict
 
 from src.domain.types import Capture
 from src.domain.interfaces.IDBMetadata import IDBMetadata
 
 
 def capture_related_records(db_metadata: IDBMetadata, records: List[Any]) -> Capture:
-    metadata: Capture = defaultdict(dict)
+    capture: Capture = defaultdict(dict)
     for record in records:
-        __capture_related_records(db_metadata, record, metadata)
-    return dict(metadata)
+        __capture_related_records(db_metadata, record, capture)
+    return dict(capture)
 
 
 def __capture_related_records(
-    db_metadata: IDBMetadata, record: Any, metadata: Capture
+    db_metadata: IDBMetadata, record: Any, capture: Capture
 ) -> None:
     table_name = db_metadata.get_table_name_from_record(record)
     record_id = db_metadata.get_record_id(record)
-    if metadata.get((table_name, record_id)):
+    if capture.get((table_name, record_id)):
         return
 
     columns = db_metadata.get_table_columns_from_record(record)
 
-    __capture_record_columns(
-        db_metadata, record, metadata, table_name, record_id, columns
+    capture[table_name, record_id] = __extract_column_values(
+        db_metadata, record, columns
     )
 
     related_records = db_metadata.get_related_records(record)
     for related_record in related_records:
-        __capture_related_records(db_metadata, related_record, metadata)
+        __capture_related_records(db_metadata, related_record, capture)
 
 
 def capture_records(db_metadata: IDBMetadata, records: List[Any]) -> Capture:
-    metadata: Capture = defaultdict(dict)
+    capture: Capture = defaultdict(dict)
     for record in records:
         table_name = db_metadata.get_table_name_from_record(record)
         record_id = db_metadata.get_record_id(record)
         columns = db_metadata.get_table_columns_from_record(record)
 
-        __capture_record_columns(
-            db_metadata, record, metadata, table_name, record_id, columns
+        capture[table_name, record_id] = __extract_column_values(
+            db_metadata, record, columns
         )
 
-    return dict(metadata)
+    return dict(capture)
 
 
 def capture_all_records(db_metadata: IDBMetadata, page_size: int = 1) -> Capture:
     tables = db_metadata.get_tables()
-    metadata: Capture = defaultdict(dict)
+    capture: Capture = defaultdict(dict)
 
     for table in tables:
         offset = 0
@@ -59,21 +59,23 @@ def capture_all_records(db_metadata: IDBMetadata, page_size: int = 1) -> Capture
             for record in records:
                 record_id = db_metadata.get_record_id(record)
 
-                __capture_record_columns(
-                    db_metadata, record, metadata, table_name, record_id, columns
+                capture[table_name, record_id] = __extract_column_values(
+                    db_metadata, record, columns
                 )
 
             offset += page_size
 
-    return dict(metadata)
+    return dict(capture)
 
 
-def __capture_record_columns(
-    db_metadata, record, metadata, table_name, record_id, columns
-):
+def __extract_column_values(
+    db_metadata: IDBMetadata, record: Any, columns: List[Any]
+) -> Dict[str, Any]:
+    columns_values: Dict[str, Any] = defaultdict(dict)
     for column in columns:
         column_name = db_metadata.get_column_name(column)
         column_value = db_metadata.get_column_value(column_name, record)
         column_is_foreign_key = db_metadata.column_is_foreign_key(column)
         key = f"{column_name} (FK)" if column_is_foreign_key else column_name
-        metadata[table_name, record_id][key] = column_value
+        columns_values[key] = column_value
+    return dict(columns_values)
